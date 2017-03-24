e155ea264d7178da183e30898becef91878ed998
John T. Wodder II <git@varonathe.org>
2017-03-24 21:05:27 +0000

    Use click

--- TODO.md
+++ TODO.md
@@ -11,6 +11,8 @@
     - showing download counts
     - getting the PyPI users associated with a package (via the XML-RPC API) ?
     - listing all of a user's packages (via the XML-RPC API)
+    - output an array of JSON objects instead of a stream
+    - controlling whether to add `people`/remove `author*` and `maintainer*` ?
 - Support getting details for a specific version (at `/$pkg/$version/json`)
   rather than the latest one
 - Parse `requires_dist` entries?
--- qypi.py
+++ qypi.py
@@ -2,10 +2,9 @@
 # Unlike the XML-RPC API, the JSON API accepts package names regardless of
 # normalization
 
-#__requires__ = ['packaging']
 import json
-import sys
 #from   packaging.version import parse
+import click
 import requests
 
 ENDPOINT = 'https://pypi.python.org/pypi'
@@ -14,41 +13,47 @@ ENDPOINT = 'https://pypi.python.org/pypi'
 #def is_prerelease(v):
 #    return parse(v).is_prerelease
 
-s = requests.Session()
-for pkgname in sys.argv[1:]:
-    r = s.get(ENDPOINT + '/' + pkgname + '/json')
-    if r.status_code == 404:
-        print('{}: {}: package not found'.format(sys.argv[0], pkgname),
-              file=sys.stderr)
-    r.raise_for_status()
-    about = r.json()
-    pkg = about["info"]
-    releases = about["releases"]
+@click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument('packages', nargs=-1)
+@click.pass_context
+def qypi(ctx, packages):
+    s = requests.Session()
+    ok = True
+    for pkgname in packages:
+        r = s.get(ENDPOINT + '/' + pkgname + '/json')
+        if r.status_code == 404:
+            click.echo('qypi: {}: package not found'.format(pkgname), err=True)
+            ok = False
+            continue
+        r.raise_for_status()
+        about = r.json()
+        pkg = about["info"]
+        #releases = about["releases"]
+        for k,v in list(pkg.items()):
+            if k.startswith(('cheesecake', '_pypi')):
+                del pkg[k]
+            elif v in ('', 'UNKNOWN'):
+                pkg[k] = None
+        pkg.pop('description', None)
+        pkg.pop('downloads', None)
+        pkg["url"] = pkg.pop('home_page', None)
+        pkg["release_date"] = min((obj["upload_time"] for obj in about["urls"]),
+                                  default=None)
+        pkg["people"] = []
+        if pkg.get('author') or pkg.get('author_email'):
+            pkg["people"].append({
+                "name": pkg.get('author'),
+                "email": pkg.get('author_email'),
+                "role": "author",
+            })
+        if pkg.get('maintainer') or pkg.get('maintainer_email'):
+            pkg["people"].append({
+                "name": pkg.get('maintainer'),
+                "email": pkg.get('maintainer_email'),
+                "role": "maintainer",
+            })
+        print(json.dumps(pkg, sort_keys=True, indent=4))
+    ctx.exit(0 if ok else 1)
 
-    for k,v in list(pkg.items()):
-        if k.startswith(('cheesecake', '_pypi')):
-            del pkg[k]
-        elif v in ('', 'UNKNOWN'):
-            pkg[k] = None
-
-    pkg.pop('description', None)
-    pkg.pop('downloads', None)
-    pkg["url"] = pkg.pop('home_page', None)
-    pkg["release_date"] = min((obj["upload_time"] for obj in about["urls"]),
-                              default=None)
-
-    pkg["people"] = []
-    if pkg.get('author') or pkg.get('author_email'):
-        pkg["people"].append({
-            "name": pkg.get('author'),
-            "email": pkg.get('author_email'),
-            "role": "author",
-        })
-    if pkg.get('maintainer') or pkg.get('maintainer_email'):
-        pkg["people"].append({
-            "name": pkg.get('maintainer'),
-            "email": pkg.get('maintainer_email'),
-            "role": "maintainer",
-        })
-
-    print(json.dumps(pkg, sort_keys=True, indent=4))
+if __name__ == '__main__':
+    qypi()
